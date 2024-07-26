4d800d296ae9bf1c369a1515c69b916fed66e28e
Shyam Sundar Durai Pandian <shyamsundar.duraipandian@mastercard.com>
2024-07-26 19:35:06 +0200

    Initial commit with more test cases with error handling

--- src/test/java/eu/sportradar/scoreboard/ScoreBoardTest.java
+++ src/test/java/eu/sportradar/scoreboard/ScoreBoardTest.java
@@ -17,10 +17,54 @@ public class ScoreBoardTest {
     }
 
     @Test
+    public void testTeamAlreadyPlaying() {
+        scoreBoard.startMatch("Mexico", "Canada");
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.startMatch("Mexico", "Brazil"),
+                "Expected startMatch() to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("One of the teams is already playing a match"));
+    }
+
+    @Test
     public void testUpdateScore() {
         scoreBoard.startMatch("Mexico", "Canada");
         scoreBoard.updateScore("Mexico", "Canada", 0, 5);
         assertEquals("Mexico 0 - Canada 5", scoreBoard.getSummary().get(0).toString());
+    }
+
+    @Test
+    public void testUpdateScoreWithoutStarting() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.updateScore("Mexico", "Canada", 0, 5),
+                "Expected updateScore() to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("Match not found"));
+    }
+
+    @Test
+    public void testUpdateScoreWithLesserValue() {
+        scoreBoard.startMatch("Mexico", "Canada");
+        scoreBoard.updateScore("Mexico", "Canada", 3, 2);
+
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.updateScore("Mexico", "Canada", 2, 1),
+                "Expected updateScore() to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("New scores must be greater than or equal to current scores"));
+    }
+
+    @Test
+    public void testUpdateScoreWithInterchangedTeams() {
+        scoreBoard.startMatch("Mexico", "Canada");
+        scoreBoard.updateScore("Mexico", "Canada", 3, 2);
+        scoreBoard.updateScore("Canada", "Mexico", 4, 3);
+
+        List<Match> matches = scoreBoard.getSummary();
+        assertEquals("Mexico 3 - Canada 4", matches.get(0).toString());
     }
 
     @Test
